ceb7f294f366f77371d0708b5b9824f1dc8e0184
Josiah Marshall <josiahtmarshall@gmail.com>
2026-03-05 15:07:19 -0500

    access_monitor.py

--- access_monitor.py
+++ access_monitor.py
@@ -2,13 +2,34 @@ import json
 import csv
 from datetime import datetime
 
-// Load the AWS access data
+  # Load the AWS access data
 with open("aws_users.json") as f:
   aws_users = json.load(f)
 
-// Load HR system data
+  # Load HR system data
 with open("hr_users.json") as f:
   hr_users = json.load(f)
+
+// Convert HR users to directory
+hr_lookup = {user["username"]: user["status"] for user in hr_users}
+
+alerts =[]
+access_inventory =[]
+
+for users in aws_users:
+
+  username = user["username"]
+  privilege = user["privilege"]
+  last_login = user["last_login_days"]
+
+  hr_status = hr_lookup.get(username)
+
+  access_inventory.append({
+    "username": username,
+    "privilege": privilege,
+    "last_login_days": last_login,
+    "hr_status": hr_status
+  })
 
   // 
           
